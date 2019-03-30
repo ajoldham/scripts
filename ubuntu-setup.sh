052f1c800524357e667c45e7524b64542d606df6
ajoldham <35148265+ajoldham@users.noreply.github.com>
2019-03-30 00:33:59 -0700

    .

---
+++ ubuntu-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Works on Azure/Ubuntu 16.04
+
+# Install Docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo apt-get install docker-compose -y
+sudo gpasswd -a $USER docker
+mkdir ~/docker
+cd ~/docker
+git clone https://github.com/ajoldham/pantools
+echo "alias dpwd='docker run -it -v `pwd`:/pwd pantools'" >> ~/.bash_aliases
+echo "alias dc='docker-compose'" >> ~/.bash_aliases
+source ~/.bash_aliases
+
+sudo apt-get install traceroute
+
+
+# Allow SSH for Ubuntu Desktop
+# sudo apt-get install openssh-server
+# sudo service ssh start
+
